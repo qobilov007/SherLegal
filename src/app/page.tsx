3d0586f37f863170e11667d13d2124c7f9aa2c5f
Javohir <yusupovjavoxir11@gmail.com>
2025-08-18 18:12:15 +0500

    fix two version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="text-[50px]">10K</h1>
-      <h2 className="text-[50px]">Kdougsaudoaspdas</h2>
+    <div className="home-prev">
+      
     </div>
   );
 }
