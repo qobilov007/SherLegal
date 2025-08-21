c0533080563244f6a413cb7d7d379529db4557e7
Javohir <yusupovjavoxir11@gmail.com>
2025-08-21 23:25:42 +0500

    fix all pages 3

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,7 +1,12 @@
+"use client"
+import Image from "next/image";
+import LoadingLogo from "../../../public/images/loading.svg"
+
+
 export default function Loading() {
     return (
-      <div className="fixed top-0 left-0 overflow-hidden w-full h-full z-50 flex items-center justify-center bg-white">
-        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      <div className="fixed top-0 left-0 overflow-hidden w-full h-full z-50 flex items-center justify-center bg-black">
+        <Image src={LoadingLogo} alt="Loading Logo" />
       </div>
     );
   }
--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Logo from "../../../public/Logo.svg";
 import Link from "next/link";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function FooterPage() {
+  const t = useTranslations("Navbarist");
+  const locale = useLocale();
   return (
-    <div className=" w-full footer-overlay">
+    <div className="w-full footer-overlay">
       <div className="container">
-        <article className="relative z-[11] max-w-[730px] m-auto md:mb-[129px] sm:mb-10 mb-6">
+        <article className="relative z-[11] pt-24 max-w-[730px] m-auto md:mb-[129px] sm:mb-10 mb-6">
           <h3 className="font-inter font-bold md:text-[64px] text-[32px] md:leading-[76px] leading-[100%] text-white text-center mb-6">
             Law and Attorney Firm in Tashkent -SherLegal
           </h3>
@@ -26,7 +29,11 @@ export default function FooterPage() {
       </div>
       <div className="container relative z-[11] w-full grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-24 gap-8">
         <div>
-          <Image className="md:mb-6 mb-3 max-md:w-[100px] max-md:h-[40px]" src={Logo} alt="Logo SherLegal" />
+          <Image
+            className="md:mb-6 mb-3 max-md:w-[100px] max-md:h-[40px]"
+            src={Logo}
+            alt="Logo SherLegal"
+          />
           <p className="font-inter font-normal text-[16px] leading-[22px] text-white/60 md:mb-9 mb-4">
             Our law firm combines legal expertise with personalized solutions to
             protect your rights and achieve your goals.
@@ -119,82 +126,95 @@ export default function FooterPage() {
             <p className="font-inter font-bold text-[14px] uppercase text-white mb-4">
               Company
             </p>
-            <li className="flex items-center gap-2 group cursor-pointer mb-3">
-              <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
-                <path
-                  d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
-                  fill="white"
-                  className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
-                />
-              </svg>
-              <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
-                Home
-              </span>
+            <li>
+              <Link
+                href={`/${locale}/about`}
+                className="flex items-center gap-2 group cursor-pointer mb-3"
+              >
+                <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
+                  <path
+                    d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
+                    fill="white"
+                    className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
+                  />
+                </svg>
+                <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
+                  {t("about")}
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={`/${locale}/services`}
+                className="flex items-center gap-2 group cursor-pointer mb-3"
+              >
+                <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
+                  <path
+                    d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
+                    fill="white"
+                    className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
+                  />
+                </svg>
+                <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
+                  {t("services")}
+                </span>
+              </Link>
             </li>
             <li className="flex items-center gap-2 group cursor-pointer mb-3">
-              <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
-                <path
-                  d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
-                  fill="white"
-                  className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
-                />
-              </svg>
-              <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
-                About us
-              </span>
-            </li>
-            <li className="flex items-center gap-2 group cursor-pointer mb-3">
-              <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
-                <path
-                  d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
-                  fill="white"
-                  className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
-                />
-              </svg>
-              <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
-                Services
-              </span>
-            </li>
-            <li className="flex items-center gap-2 group cursor-pointer">
-              <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
-                <path
-                  d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
-                  fill="white"
-                  className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
-                />
-              </svg>
-              <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
-                Our team
-              </span>
+              <Link
+                href={`/${locale}/team`}
+                className="flex items-center gap-2 group cursor-pointer mb-3"
+              >
+                <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
+                  <path
+                    d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
+                    fill="white"
+                    className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
+                  />
+                </svg>
+                <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
+                  {t("ourteam")}
+                </span>
+              </Link>
             </li>
           </ul>
           <ul>
             <p className="font-inter font-bold text-[14px] uppercase text-white mb-4">
               support
             </p>
-            <li className="flex items-center gap-2 group cursor-pointer mb-3">
-              <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
-                <path
-                  d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
-                  fill="white"
-                  className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
-                />
-              </svg>
-              <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
-                Terms of use
-              </span>
+            <li>
+              <Link
+                href={`/${locale}/terms-of-use`}
+                className="flex items-center gap-2 group cursor-pointer mb-3"
+              >
+                <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
+                  <path
+                    d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
+                    fill="white"
+                    className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
+                  />
+                </svg>
+                <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
+                  Terms of use
+                </span>
+              </Link>
             </li>
-            <li className="flex items-center gap-2 group cursor-pointer">
-              <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
-                <path
-                  d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
-                  fill="white"
-                  className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
-                />
-              </svg>
-              <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
-                Privacy policy
-              </span>
+            <li className="flex items-center gap-2 group cursor-pointer mb-3">
+              <Link
+                href={`/${locale}/privacy-policy`}
+                className="flex items-center gap-2 group cursor-pointer mb-3"
+              >
+                <svg width="9" height="13" viewBox="0 0 9 13" fill="none">
+                  <path
+                    d="M8.18332 0.5H0.859462C0.859462 0.5 0.81611 0.502296 0.794772 0.507655C0.753452 0.517606 0.697906 0.53789 0.640667 0.579608L0.638297 0.581139C0.550576 0.645437 0.50011 0.758342 0.50011 0.876606V8.41025C0.499433 8.47531 0.501465 8.58056 0.517722 8.70763C0.600363 9.35674 0.987487 10.0518 3.02641 11.5425C3.36036 11.7867 3.80709 12.1021 4.34798 12.4526C4.44485 12.5154 4.56441 12.5158 4.66161 12.4538C5.06161 12.1989 5.38912 11.9792 5.62282 11.82C7.95742 10.2259 8.37198 9.54274 8.47358 8.89593C8.49695 8.74743 8.5 8.62534 8.5 8.55875V0.857852C8.5 0.660364 8.35809 0.5 8.18332 0.5Z"
+                    fill="white"
+                    className="group-hover:fill-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100"
+                  />
+                </svg>
+                <span className="text-white font-inter text-[14px] font-normal group-hover:text-[#137BEA] transition-all duration-200 opacity-50 group-hover:opacity-100">
+                  Privacy policy
+                </span>
+              </Link>
             </li>
           </ul>
         </article>
